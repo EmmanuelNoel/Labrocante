42e9e0858fd6f1d96ca891193a1e7146a8373152
Emmanuel <EmmanuelNoel>
2024-02-09 17:57:51 +0100

    display recent articles at home

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produit;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     //
 
-    public function index(){
-        return view('home');
+    public function index()
+    {
+        $produits_recents = Produit::with('category', 'medias')->orderBy('created_at', 'desc')->take(8)->get();
+      return view('home', compact('produits_recents'));
     }
 }
